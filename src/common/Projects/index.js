be6eadd1dbe173e7108c65f8533f6d5c2648f80a
MYKYTA RYSIEV <nickpolska21@gmail.com>
2022-01-24 17:26:13 +0100

    links to translations added

--- src/common/Projects/index.js
+++ src/common/Projects/index.js
@@ -5,12 +5,12 @@ import { Links, Link, Button } from "./styled";
 const Projects = () => (
   <Links>
     <Clock />
-    <Link href="ToDo-List--EN-CRMS--ReactApp"><Button>EN</Button></Link>
-    <Link href="ToDo-List--DE-CRMS--ReactApp"><Button>DE</Button></Link>
-    <Link href="ToDo-List--ES-CRMS--ReactApp"><Button>ES</Button></Link>
-    <Link href="ToDo-List--RU-CRMS--ReactApp"><Button>RU</Button></Link>
-    <Link href="ToDo-List--UA-CRMS--ReactApp"><Button>UA</Button></Link>
-    <Link href="#"><Button deepSkyBlue>🌓</Button></Link>
+    <Link href={"ToDo-List--EN-CRMS--ReactApp"}><Button>EN</Button></Link>
+    <Link href={"ToDo-List--DE-CRMS--ReactApp"}><Button>DE</Button></Link>
+    <Link href={"ToDo-List--ES-CRMS--ReactApp"}><Button>ES</Button></Link>
+    <Link href={"ToDo-List--RU-CRMS--ReactApp"}><Button>RU</Button></Link>
+    <Link href={"ToDo-List--UA-CRMS--ReactApp"}><Button>UA</Button></Link>
+    <Link href={"#"}><Button deepSkyBlue>🌓</Button></Link>
   </Links>
 )
 
